8df22b4a3535b7076d74824076f2577e990dbf09
Rafael Braga <rafaelp.ceciliano@gmail.com>
2018-07-09 21:09:37 -0300

    configurando husks

--- api-avaliacao/src/service/perguntaService.js
+++ api-avaliacao/src/service/perguntaService.js
@@ -1,6 +1,6 @@
-const Grupo = require('../model/grupo')
+const Grupo = require('../model/grupo');
 
-Grupo.methods(['get', 'post', 'put', 'delete'])
-Grupo.updateOptions({new: true, runValidators: true})
+Grupo.methods(['get', 'post', 'put', 'delete']);
+Grupo.updateOptions({new: true, runValidators: true});
 
-module.exports = Grupo
+module.exports = Grupo;
--- api-avaliacao/src/service/respostaService.js
+++ api-avaliacao/src/service/respostaService.js
@@ -1,12 +1,11 @@
-const Grupo = require('../model/grupo')
+const Grupo = require('../model/grupo');
 
 module.exports = function(router){
-
     router.route('/respostas/lista/:id').get((req, res, next) => {
         Grupo.find({'perguntas._id': req.params.id})
         .then(grupo => res.status(200).json({ grupo }))
         .catch(err => res.status(400).json(err))
-    })
+    });
 
     router.route('/respostas/inserir/:id').post((req, res, next) => {
         Grupo.findOne({'perguntas._id': req.params.id})
@@ -14,6 +13,6 @@ module.exports = function(router){
             grupo.perguntas[0].respostas.push({"teamId":"4","status":"pending"});
             console.log(grupo);
         })
-        .catch(err => res.status(400).json(err))
-    })
-}
+        .catch(err => res.status(400).json(err));
+    });
+};
