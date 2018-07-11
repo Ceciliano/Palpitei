4cf91e6846155804cf40850719b8201c34bc5cdd
Rafael Braga <rafaelp.ceciliano@gmail.com>
2018-07-11 00:40:58 -0300

    Configurando desenvolvimento

--- api-avaliacao/src/service/respostaService.js
+++ api-avaliacao/src/service/respostaService.js
@@ -1,13 +1,13 @@
 const Grupo = require('../model/grupo');
 
 module.exports = function(router) {
-  router.route( '/respostas/lista/:id' ).get((req, res, next) => {
+  router.route( '/respostas/lista/:id' ).get((req, res) => {
     Grupo.find({'perguntas._id': req.params.id})
     .then((grupo) => res.status(200).json({grupo}))
     .catch((err) => res.status(400).json(err));
   });
 
-  router.route('/respostas/inserir/:id').post((req, res, next) => {
+  router.route('/respostas/inserir/:id').post((req, res) => {
     Grupo.findOne({'perguntas._id': req.params.id})
     .then((grupo) => {
       grupo.perguntas[0].respostas.push({"teamId": "4", "status": "pending"});
